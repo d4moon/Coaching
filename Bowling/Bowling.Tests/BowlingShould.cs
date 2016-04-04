b52cb13bf2c0725bfdfba9440884fd130bd2a93c
Alessandro Di Gioia <Alessandro.DiGioia@asos.com>
2016-04-04 09:07:30 +0100

    Added Bowling place holder

---
+++ Bowling/Bowling.Tests/BowlingShould.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Bowling.Tests
+{
+    [TestFixture]
+    public class BowlingShould
+    {
+        [Test]
+        public void Fail()
+        {
+            Assert.Fail();
+        }
+    }
+}
