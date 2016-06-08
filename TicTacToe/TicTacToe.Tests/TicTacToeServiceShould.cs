a6c2ac01cdb8e4da4a9e4e9340270e745399abbb
Damon Serji <Damon.Serji@asos.com>
2016-06-08 11:50:52 +0100

    refactored tests

--- TicTacToe/TicTacToe.Tests/TicTacToeServiceShould.cs
+++ TicTacToe/TicTacToe.Tests/TicTacToeServiceShould.cs
@@ -18,17 +18,22 @@ namespace TicTacToe.Tests
         [TestCase("XOX      ", new[] { 1, 1, 1, 2, 1, 3 })]
         [TestCase("X        ", new[] { 1, 1 })]
         [TestCase("XO       ", new[] { 1, 1, 1, 2 })]
-        public void allocate_correct_moves_for_given_input(string expStuff, int[] moves)
+        public void allocate_correct_moves_for_given_input(string expectedBoard, int[] moves)
         {
-            string gameResult = null;
-            for (int i = 0; i < moves.Length; i = i + 2)
-            {
-                gameResult = _ticTacToe.Play(new Row(moves[i]), new Column(moves[i+1]));
-            }
+            var renderedBoard = PlayMoves(moves);
 
-            gameResult.Should().Be(expStuff);
+            renderedBoard.Should().Be(expectedBoard);
         }
 
+        private string PlayMoves(int[] moves)
+        {
+            string renderedBoard = null;
+            for (int i = 0; i < moves.Length; i = i + 2)
+            {
+                renderedBoard = _ticTacToe.Play(new Row(moves[i]), new Column(moves[i + 1]));
+            }
+            return renderedBoard;
+        }
     }
 
 
