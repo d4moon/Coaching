ad201f40d048a8173b55467234b29ac4d6d1111b
Damon.Serji <Damon.Serji@ASGLH-WL-10747>
2016-07-31 03:19:31 +0100

    test source is not null

---
+++ CharacterCopier/CharacterCopier.Test/CharacterCopierShould.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CharacterCopier.Test
+{
+    [TestFixture]
+    class CharacterCopierShould
+    {
+        [Test]
+        public void Throw_an_exception_given_source_is_null()
+        {
+            Action action = () => new CharacterCopier(null, null);
+            action.ShouldThrow<ArgumentNullException>();
+        }
+    }
+
+    internal class CharacterCopier
+    {
+        public CharacterCopier(ISource source, IDestination destination)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+
+    internal interface ISource
+    {
+    }
+
+    internal interface IDestination
+    {
+    }
+}
