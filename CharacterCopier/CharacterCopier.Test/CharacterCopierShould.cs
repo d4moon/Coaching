29e0c4873a1f080583de9290b16d277905874537
Damon.Serji <Damon.Serji@ASGLH-WL-10747>
2016-08-10 08:15:40 +0100

    refactoring tests and classes

--- CharacterCopier/CharacterCopier.Test/CharacterCopierShould.cs
+++ CharacterCopier/CharacterCopier.Test/CharacterCopierShould.cs
@@ -9,10 +9,22 @@ namespace CharacterCopier.Test
     [TestFixture]
     class CharacterCopierShould
     {
+        private const char A_NEW_LINE_CHAR = '\n';
+        private const char A_CHARACTER = 'A';
+        private Mock<IDestination> _destinationMock;
+        private Mock<ISource> _sourceMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _destinationMock = new Mock<IDestination>();
+            _sourceMock = new Mock<ISource>();
+        }
+
         [Test]
         public void Throw_an_exception_given_source_is_null()
         {
-            Action action = () => new CharacterCopier(null, new DestinationMock());
+            Action action = () => new CharacterCopier(null, _destinationMock.Object);
             action.ShouldThrow<ArgumentNullException>();
         }
         
@@ -26,63 +38,34 @@ namespace CharacterCopier.Test
         [Test]
         public void Get_character_from_source_once_given_source_returned_newline()
         {
-            var sourceMock = new Mock<ISource>();
-            sourceMock.Setup(s => s.GetChar()).Returns('\n');
-            var characterCopier = new CharacterCopier(sourceMock.Object, new DestinationMock());
+            _sourceMock.Setup(s => s.GetChar()).Returns(A_NEW_LINE_CHAR);
+            var characterCopier = new CharacterCopier(_sourceMock.Object, _destinationMock.Object);
 
             characterCopier.Copy();
 
-            sourceMock.Verify(s => s.GetChar(), Times.Once());
+            _sourceMock.Verify(s => s.GetChar(), Times.Once());
         }
 
         [Test]
         public void Get_all_characters_from_source_given_source_returnes_characters()
         {
-            var sourceMock = new Mock<ISource>();
-            sourceMock.SetupSequence(s => s.GetChar()).Returns('A').Returns('B').Returns('\n');
-            var characterCopier = new CharacterCopier(sourceMock.Object, new DestinationMock());
+            _sourceMock.SetupSequence(s => s.GetChar()).Returns(A_CHARACTER).Returns(A_CHARACTER).Returns(A_NEW_LINE_CHAR);
+            var characterCopier = new CharacterCopier(_sourceMock.Object, _destinationMock.Object);
 
             characterCopier.Copy();
 
-            sourceMock.Verify(s => s.GetChar(), Times.Exactly(3));
+            _sourceMock.Verify(s => s.GetChar(), Times.Exactly(3));
         }
 
         [Test]
         public void Not_copy_characters_to_destination_given_character_is_new_line()
         {
-            var A_NEW_LINE_CHAR = '\n';
-            var destinationMock = new Mock<IDestination>();
-            var sourceMock = new Mock<ISource>();
-            sourceMock.Setup(s => s.GetChar()).Returns(A_NEW_LINE_CHAR);
-            var characterCopier = new CharacterCopier(sourceMock.Object, destinationMock.Object);
+            _sourceMock.Setup(s => s.GetChar()).Returns(A_NEW_LINE_CHAR);
+            var characterCopier = new CharacterCopier(_sourceMock.Object, _destinationMock.Object);
 
             characterCopier.Copy();
 
-            destinationMock.Verify(s => s.SetChar(A_NEW_LINE_CHAR), Times.Never());
+            _destinationMock.Verify(s => s.SetChar(A_NEW_LINE_CHAR), Times.Never());
         }
-
-        [Test]
-        public void Copy_received_characters_to_destination_given_valid_destination()
-        {
-            
-        }
-    }
-    
-    internal class DestinationMock : IDestination
-    {
-        public void SetChar(char character)
-        {
-            
-        }
-    }
-
-    internal interface ISource
-    {
-        char GetChar();
-    }
-
-    internal interface IDestination
-    {
-        void SetChar(char character);
     }
 }
