6b7af496b49b5eb10df1019cdadbdab7c8fb5789
Damon.Serji <Damon.Serji@ASGLH-WL-10747>
2016-08-11 07:35:23 +0100

    check instrument and taskDistpacher are not null

--- InstrumentProcessor/InstrumentProcessorTests/Class1.cs
+++ InstrumentProcessor/InstrumentProcessorTests/Class1.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+using Moq;
+using Nerdle.Ensure;
 
 namespace InstrumentProcessorTests
 {
-    public class Class1
+    [TestFixture]
+    public class Constructing_instrument_processor_should
     {
+        [Test]
+        public void Throw_an_exception_given_no_task_dispatcher()
+        {
+            var instrument = new Mock<IInstrument>();
+            Action action = () => new InstrumentProcessor(null, instrument.Object);
+
+            action.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Test]
+        public void Throw_an_exception_given_no_instrument()
+        {
+            var taskDispatcher = new Mock<ITaskDispatcher>();
+
+            Action action = () => new InstrumentProcessor(taskDispatcher.Object, null);
+
+            action.ShouldThrow<ArgumentNullException>();
+        }
+
+    }
+
+    public interface IInstrument
+    {
+    }
+
+    public class InstrumentProcessor
+    {
+        public InstrumentProcessor(ITaskDispatcher taskDispatcher, IInstrument instrument)
+        {
+            Ensure.Argument(taskDispatcher, nameof(taskDispatcher)).NotNull();
+            Ensure.Argument(instrument, nameof(instrument)).NotNull();
+        }
+    }
+
+    public interface ITaskDispatcher
+    {
+
     }
 }
