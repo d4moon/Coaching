0f2a5dadfcfa86c1c524d8c06335a7e073e9de83
Damon.Serji <Damon.Serji@ASGLH-WL-10747>
2016-08-11 07:50:17 +0100

    test task is retrieved when processing instrumentProcessor

--- InstrumentProcessor/InstrumentProcessorTests/Class1.cs
+++ InstrumentProcessor/InstrumentProcessorTests/Class1.cs
@@ -29,17 +29,37 @@ namespace InstrumentProcessorTests
         }
     }
 
+    [TestFixture]
+    class Instrument_processor_should
+    {
+        [Test]
+        public void Get_a_task_when_processing()
+        {
+            var taskDispatcher = new Mock<ITaskDispatcher>();
+            var instrument = new Mock<IInstrument>();
+
+            new InstrumentProcessor(taskDispatcher.Object, instrument.Object); 
+
+            taskDispatcher.Verify(t => t.GetTask(), Times.Once());
+        }
+    }
+
     public class InstrumentProcessor: IInstrumentProcessor
     {
+        private readonly ITaskDispatcher _taskDispatcher;
+
         public InstrumentProcessor(ITaskDispatcher taskDispatcher, IInstrument instrument)
         {
+            _taskDispatcher = taskDispatcher;
             Ensure.Argument(taskDispatcher, nameof(taskDispatcher)).NotNull();
             Ensure.Argument(instrument, nameof(instrument)).NotNull();
+
+            Process();
         }
 
         public void Process()
         {
-            
+            _taskDispatcher.GetTask();
         }
     }
 
